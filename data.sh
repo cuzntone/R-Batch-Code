b8f9152962211aea59db993f1268d8940616770e
cuzntone <cuzntone@users.noreply.github.com>
2015-02-24 20:31:38 -0500

    Update data.sh
    Command to submit data.sh to create 500 data sets.
    qsub -t 1-500 data.sh

--- data.sh
+++ data.sh
@@ -3,5 +3,5 @@
 #PBS -q planck
 source /share/apps/Modules/3.2.10/init/sh
 module load R/3.1.0
-R --vanilla < /home/tongx/hello_world/data.r >  /home/tongx/hello_world/output/sample_$PBS_ARRAYID.txt
+R --vanilla < /home/tongx/data.r >  /home/tongx/output/sample_$PBS_ARRAYID.txt
 
